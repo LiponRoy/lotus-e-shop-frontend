b07e046f9dec7a2aa990019991814b6ec9793f69
Lipon Roy <liponbdbd@gmail.com>
2023-01-07 16:39:49 +0600

    configured a new reactJs project with tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,24 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1 className='text-4xl text-white font-bold bg-red-500 p-4'>Lotus E shop project (Lipon Roy)</h1>
+		</div>
+	);
 }
 
 export default App;
