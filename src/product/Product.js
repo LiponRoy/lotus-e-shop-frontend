907bbd8d33cd63a334a9f939747e8ac97b4eeeb6
Lipon Roy <liponbdbd@gmail.com>
2023-02-11 12:33:41 +0600

    created Search filter option

--- src/product/Product.js
+++ src/product/Product.js
@@ -9,7 +9,7 @@ import { productsFetch } from '../features/products/productsSlice';
 const Product = () => {
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
-	//const { data, isLoading, isSuccess, isError } = useGetProductTasksQuery();
+
 	const { items, status, createStatus } = useSelector((state) => state.products);
 
 	useEffect(() => {
@@ -18,6 +18,10 @@ const Product = () => {
 		}
 		console.log(items);
 	}, [items, dispatch]);
+
+	// for search item
+	const [search, setSearch] = useState('');
+	console.log(search);
 
 	// for category
 	const [data, setData] = useState(items);
@@ -52,30 +56,36 @@ const Product = () => {
 	return (
 		<div>
 			<div className='w-full text-center text-2xl md:text-2xl font-bold'>
-				<div className=''>OUR LATEST PRODUCT</div>
-				<div className='text-xl'>Total Item {data.length}</div>
+				{/* <div className='text-xl'>Total Item {data.length}</div> */}
+				<div className=' text-2xl m-1'>OUR LATEST PRODUCT </div>
+				<div className='text-md'>Total Item : {data.length}</div>
 			</div>
 			<section className='text-gray-600 body-font'>
 				<div className='container mx-auto flex px-2  md:flex-row flex-col items-center'>
 					<div className='lg:max-w-lg lg:w-64 lg:h-screen'>
-						<div className=' flex flex-col items-center justify-start text-blue-800'>
-							<span className='mb-2 text-2xl '>categories</span>
-							<button className='my-1 bg-[#646464] w-32 text-white' onClick={() => selectBrand('men')}>
+						<div className=' flex flex-col items-start justify-center text-blue-800'>
+							{/* search here */}
+							<span className='mb-2 mt-4 text-1xl '>Search here :</span>
+
+							<input onChange={(e) => setSearch(e.target.value)} type='text' placeholder='Search by name...' className='input input-bordered input-warning w-52 mb-4' />
+
+							<span className='mb-2 text-1xl '>Categories :</span>
+							<button className='my-1 bg-[#646464] w-52 h-8 rounded-md text-white' onClick={() => selectBrand('men')}>
 								men
 							</button>
 
-							<button className='my-1 bg-[#646464] w-32 text-white' onClick={() => selectBrand('women')}>
+							<button className='my-1 bg-[#646464] w-52 h-8 rounded-md text-white' onClick={() => selectBrand('women')}>
 								women
 							</button>
 
-							<button className='my-1 bg-[#646464] w-32 text-white' onClick={() => selectBrand('shirt')}>
+							<button className='my-1 bg-[#646464] w-52 h-8 rounded-md text-white' onClick={() => selectBrand('shirt')}>
 								shirt
 							</button>
-							<button className='my-1 bg-[#646464] w-32 text-white' onClick={() => selectBrand('tshirt')}>
+							<button className='my-1 bg-[#646464] w-52 h-8 rounded-md text-white' onClick={() => selectBrand('tshirt')}>
 								tshirt
 							</button>
 
-							<button className='my-1 bg-[#646464] w-32 text-white' onClick={() => allBrand()}>
+							<button className='my-1 bg-[#646464] w-52 h-8 rounded-md text-white' onClick={() => allBrand()}>
 								All
 							</button>
 						</div>
@@ -83,21 +93,28 @@ const Product = () => {
 					<div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center '>
 						<div className='w-auto'>
 							<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 m-12 mx-16'>
-								{data?.slice(pagesVisited, pagesVisited + usersPerPage).map((value, i) => (
-									<div key={i}>
-										<div className='relative overflow-hidden bg-no-repeat bg-cover max-w-xs'>
-											<img src={value?.image.url} width={300} className='hover:scale-110 transition duration-300 ease-in-out' alt='noImg' />
-										</div>
+								{/* here slice() for categories and filter() for search option
+								  and map() for showing data */}
+								{data
+									?.slice(pagesVisited, pagesVisited + usersPerPage)
+									.filter((item) => {
+										return search.toLowerCase() === '' ? item : item.name.toLowerCase().includes(search);
+									})
+									.map((value, i) => (
+										<div key={i}>
+											<div className='relative overflow-hidden bg-no-repeat bg-cover max-w-xs'>
+												<img src={value?.image.url} width={300} className='hover:scale-110 transition duration-300 ease-in-out' alt='noImg' />
+											</div>
 
-										<span className='text-2xl mt-2 font-semibold '>
-											{value?.price}
-											<span className='text-xl'>TK</span>
-										</span>
-										<button className='btn btn-sm btn-warning m-1 ml-2'>Buy now</button>
-										<p className='text-xl font-semibold'>{value?.name}</p>
-										<p className=''>{value?.desc}</p>
-									</div>
-								))}
+											<span className='text-2xl mt-2 font-semibold '>
+												{value?.price}
+												<span className='text-xl'>TK</span>
+											</span>
+											<button className='btn btn-sm btn-warning m-1 ml-2'>Buy now</button>
+											<p className='text-xl font-semibold'>{value?.name}</p>
+											<p className=''>{value?.desc}</p>
+										</div>
+									))}
 							</div>
 						</div>
 					</div>
