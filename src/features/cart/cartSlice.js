c3c21dd530911d1594daebb3a2bd0c7ff33cb82d
Lipon Roy <liponbdbd@gmail.com>
2023-04-02 21:34:46 +0600

    created add to cart functionality

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -9,7 +9,26 @@ const initialState = {
 export const cartSlice = createSlice({
 	name: 'cart',
 	initialState,
-	reducers: {},
+	reducers: {
+		addToCart(state, action) {
+			// if get any item then return 0 or 1 or 2 and so on
+			// but Don't get any item then it return -1
+			const index = state.cartProducts.findIndex((indexItem) => {
+				indexItem.id === action.payload.id;
+			});
+			// if it get zero or upper value
+			if (index.length >= 0) {
+				// no item added only cartTotalQuantity incress value
+				state.cartProducts[index].cartTotalQuantity += 1;
+			} else {
+				// added total value
+				const tempProduct = { ...action.payload, cartTotalQuantity: 1 };
+				state.cartProducts.push(tempProduct);
+			}
+
+			//......................
+		},
+	},
 });
 
 export const { addToCart } = cartSlice.actions;
