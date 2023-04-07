b66a7035a9753b75cd49bded701d90cdfd7050c9
Lipon Roy <liponbdbd@gmail.com>
2023-04-07 10:26:40 +0600

    decrease and increase cart quantity done

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -20,6 +20,23 @@ export const cartSlice = createSlice({
 			}
 			console.log(action.payload_id);
 		},
+		decreaseQuantity(state, action) {
+			const cartIndex = state.cartProducts.findIndex((item) => {
+				return item._id === action.payload._id;
+			});
+			console.log(cartIndex);
+			if (state.cartProducts[cartIndex].cartQuantity > 1) {
+				state.cartProducts[cartIndex].cartQuantity -= 1;
+			}
+		},
+		IncreaseQuantity(state, action) {
+			const cartIndex = state.cartProducts.findIndex((item) => {
+				return item._id === action.payload._id;
+			});
+			if (state.cartProducts[cartIndex].cartQuantity >= 1) {
+				state.cartProducts[cartIndex].cartQuantity += 1;
+			}
+		},
 		removeCart(state, action) {
 			const deleteItem = state.cartProducts.filter((product) => {
 				return product._id !== action.payload._id;
@@ -29,5 +46,5 @@ export const cartSlice = createSlice({
 	},
 });
 
-export const { addToCartTwo, removeCart } = cartSlice.actions;
+export const { addToCartTwo, removeCart, decreaseQuantity, IncreaseQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
--- src/pages/cart/CartDetaials.js
+++ src/pages/cart/CartDetaials.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { removeCart } from '../../features/cart/cartSlice';
+import { removeCart, decreaseQuantity, IncreaseQuantity } from '../../features/cart/cartSlice';
 
 const CartDetaials = () => {
 	const dispatch = useDispatch();
@@ -12,21 +12,56 @@ const CartDetaials = () => {
 	const removeItem = (item) => {
 		dispatch(removeCart(item));
 	};
+	const decrease = (item) => {
+		dispatch(decreaseQuantity(item));
+	};
+	const increase = (item) => {
+		dispatch(IncreaseQuantity(item));
+	};
 	return (
 		<div>
-			{cartProducts.length === 0 && <spn> Your Cart is Empty</spn>}
+			<div className=' flex justify-center items-center'>
+				<div className=' grid grid-cols-1 md:grid-cols-2 mt-5'>
+					<div className=' '>
+						{cartProducts.length === 0 && <spn> Your Cart is Empty</spn>}
+						{cartProducts?.map((item) => (
+							<div key={item._id} className=' flex items-center  w-full my-4'>
+								<div>
+									<img className=' rounded-md' src={item?.image.url} alt='' width='160px' height='160px' />
+								</div>
 
-			<div className='ProductsOfCart'>
-				{cartProducts?.map((item) => (
-					<div key={item._id} className='product'>
-						<img src={item?.image.url} alt='' width='212px' height='212px' />
-						<span>{item.name}</span>
-						<span>{item.brand}</span>
-						<span>{item.price}</span>
-						<span>Total Price:{item.price * item.cartQuantity}</span>
-						<button onClick={() => removeItem(item)}> REMOVE</button>
+								<div className=' flex flex-col justify-center  ml-10 '>
+									<span className=' text-xl'>{item.name}</span>
+									{/* <span>{item.brand}</span> */}
+									<span>Price: {item.price} TK</span>
+									<span>Quantity: {item.cartQuantity} </span>
+									<span>Total Price: {item.price * item.cartQuantity} TK</span>
+									<div className='mt-2 text-2xl'>
+										<button onClick={() => decrease(item)} class=' mr-4 font-medium'>
+											-
+										</button>
+										<span>{item.cartQuantity}</span>
+										<button onClick={() => increase(item)} class=' ml-4 '>
+											+
+										</button>
+									</div>
+									<button className='btn btn-sm bg-orange-600 w-20 mt-2' onClick={() => removeItem(item)}>
+										Remove
+									</button>
+								</div>
+							</div>
+						))}
 					</div>
-				))}
+					<div className='w-full flex items-start justify-center text-2xl bg-red-400 bg-slate-100 rounded'>
+						<div className='flex flex-col'>
+							<span>Bangladeshi !</span>
+							<span>Bangladeshi !</span>
+							<span>Bangladeshi !</span>
+							<span>Bangladeshi !</span>
+							<span>Bangladeshi !</span>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
